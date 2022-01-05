01f4ffb08db8038424da7466a62a9ee77f8e3e8a
Thales Matias <thales.matias@infobase.com.br>
2022-01-05 11:02:25 -0300

    Refactoring
    Avoiding invalid index and using variable instead of accessing anonymous array again.

--- src/Bootables/BootHttpRemoteCallsLog.php
+++ src/Bootables/BootHttpRemoteCallsLog.php
@@ -24,12 +24,12 @@ class BootHttpRemoteCallsLog extends AbstractBootable
                 'body' => is_string($body = ($request['body'] ?? null)) ?
                     json_decode($request['body'], true) : $body,
             ], JSON_PRETTY_PRINT);
-            $http_response = $response['http_response'];
+            $http_response = $response['http_response'] ?? null;
 
             if (!($http_response instanceof WP_HTTP_Requests_Response)) {
                 $raw_response = 'INVALID RESPONSE OBJECT STRUCTURE: ' . var_export($http_response, true);
             } else {
-                $raw_response = $response['http_response']->get_response_object()->raw;
+                $raw_response = $http_response->get_response_object()->raw;
             }
 
             error_log(self::headerLog('REMOTE API CALL')
