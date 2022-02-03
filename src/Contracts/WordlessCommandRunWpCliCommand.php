ccc70aab5f6bd8cd1dab1740e540b32441888839
Thales Matias <thales.matias@infobase.com.br>
2022-02-03 16:03:32 -0300

    Avoiding update core when version is the same

--- src/Contracts/WordlessCommandRunWpCliCommand.php
+++ src/Contracts/WordlessCommandRunWpCliCommand.php
@@ -3,11 +3,34 @@
 namespace Wordless\Contracts;
 
 use Exception;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Wordless\Commands\WpCliCaller;
 use Wordless\Exception\WpCliCommandReturnedNonZero;
 
 trait WordlessCommandRunWpCliCommand
 {
+    /**
+     * @param string $command
+     * @param bool $stop_on_error
+     * @return string
+     * @throws WpCliCommandReturnedNonZero
+     * @throws Exception
+     */
+    private function runAndGetWpCliCommandOutput(string $command, bool $stop_on_error = false): string
+    {
+        if ($this->modes[self::ALLOW_ROOT_MODE]) {
+            $command = "$command --allow-root";
+        }
+
+        if (($return_var = $this->executeWordlessCommand(WpCliCaller::COMMAND_NAME, [
+                WpCliCaller::WP_CLI_FULL_COMMAND_STRING_ARGUMENT_NAME => $command,
+            ], $wpCliOutput = new BufferedOutput)) && !$stop_on_error) {
+            throw new WpCliCommandReturnedNonZero($command, $return_var);
+        }
+
+        return $wpCliOutput->fetch();
+    }
+
     /**
      * @param string $command
      * @param bool $return_script_code
