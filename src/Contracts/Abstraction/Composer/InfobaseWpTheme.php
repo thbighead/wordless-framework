e6c7d83c7aeda5f26d69c213932a1acf8bb0c801
Thales Matias <thales.matias@infobase.com.br>
2022-07-21 17:22:41 -0300

    Finishing WP theme post install script

--- src/Contracts/Abstraction/Composer/InfobaseWpTheme.php
+++ src/Contracts/Abstraction/Composer/InfobaseWpTheme.php
@@ -4,6 +4,7 @@ namespace Wordless\Contracts\Abstraction\Composer;
 
 use Composer\Installer\PackageEvent;
 use Composer\Package\CompletePackage;
+use Symfony\Component\Dotenv\Dotenv;
 use Wordless\Exceptions\FailedToCopyFile;
 use Wordless\Exceptions\FailedToCreateDirectory;
 use Wordless\Exceptions\FailedToDeletePath;
@@ -11,6 +12,7 @@ use Wordless\Exceptions\FailedToGetDirectoryPermissions;
 use Wordless\Exceptions\InvalidDirectory;
 use Wordless\Exceptions\PathNotFoundException;
 use Wordless\Helpers\DirectoryFiles;
+use Wordless\Helpers\Environment;
 use Wordless\Helpers\ProjectPath;
 
 trait InfobaseWpTheme
@@ -28,19 +30,28 @@ trait InfobaseWpTheme
     public static function installInfobaseWpTheme(PackageEvent $composerEvent)
     {
         $composerPackage = self::extractPackageFromEvent($composerEvent);
+        self::defineProjectRootConstant(
+            dirname($composerEvent->getComposer()->getConfig()->get('vendor-dir'))
+        );
 
         if (!self::isInfobaseWpThemePackage($composerPackage)) {
             return;
         }
+
+        Environment::loadDotEnv();
 
         self::exportFilesToProjectRoot();
 
         passthru("php console theme:npm \"install\"");
     }
 
-    private static function isInfobaseWpThemePackage(CompletePackage $package): bool
+    private static function defineProjectRootConstant(string $path)
     {
-        return $package->getName() === 'infobaseit/infobase-wp-theme';
+        $root_project_path_const_name = 'ROOT_PROJECT_PATH';
+
+        if (!defined($root_project_path_const_name)) {
+            define($root_project_path_const_name, $path);
+        }
     }
 
     /**
@@ -56,5 +67,10 @@ trait InfobaseWpTheme
     {
         DirectoryFiles::recursiveCopy($setup_path = ProjectPath::theme('setup'), ProjectPath::root());
         DirectoryFiles::recursiveDelete($setup_path);
+    }
+
+    private static function isInfobaseWpThemePackage(CompletePackage $package): bool
+    {
+        return $package->getName() === 'infobaseit/infobase-wp-theme';
     }
 }
