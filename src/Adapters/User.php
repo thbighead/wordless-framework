99b9e9cb8aa26db6f9c93832d8922412709306c3
Thales Matias <thales.matias@infobase.com.br>
2022-08-25 15:14:54 -0300

    User adapter improvement
    Extending from WP_User as we wished

--- src/Adapters/User.php
+++ src/Adapters/User.php
@@ -4,21 +4,15 @@ namespace Wordless\Adapters;
 
 use WP_User;
 
-class User
+class User extends WP_User
 {
-    public ?WP_User $wp_user;
-
     public function __construct(?WP_User $wp_user = null)
     {
-        $this->wp_user = $wp_user ?? wp_get_current_user();
+        parent::__construct($wp_user ?? wp_get_current_user());
     }
 
     public function can($capability, ...$args): bool
     {
-        if (is_null($this->wp_user)) {
-            return false;
-        }
-
-        return $this->wp_user->has_cap($capability, ...$args);
+        return $this->has_cap($capability, ...$args);
     }
 }
