bc601241a3a596e528bc423a514f90fc41e57502
Thales Matias <thales.matias@infobase.com.br>
2022-08-29 12:48:16 -0300

    Implementing AcfProvider
    Only add hooks if package is installed

--- src/Abstractions/AcfProvider.php
+++ src/Abstractions/AcfProvider.php
@@ -2,7 +2,23 @@
 
 namespace Wordless\Abstractions;
 
-class Acf
-{
+use Composer\InstalledVersions;
+use Wordless\Hookers\AcfAutoGeneratePhpCodeOnSave;
+use Wordless\Hookers\AcfLoadLocalGroups;
+use Wordless\Hookers\HideAcfAdminPanelAtProduction;
 
+class AcfProvider
+{
+    public static function addAdditionalHooks(): array
+    {
+        if (!InstalledVersions::isInstalled('infobaseit/acf-pro')) {
+            return [];
+        }
+
+        return [
+            AcfAutoGeneratePhpCodeOnSave::class,
+            AcfLoadLocalGroups::class,
+            HideAcfAdminPanelAtProduction::class,
+        ];
+    }
 }
