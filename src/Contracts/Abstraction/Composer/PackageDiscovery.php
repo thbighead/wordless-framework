c218044d1edfeccc9c81b8bfd8e1de184e9af774
Thales Matias <thales.matias@infobase.com.br>
2022-09-01 15:55:36 -0300

    WIP

--- src/Contracts/Abstraction/Composer/PackageDiscovery.php
+++ src/Contracts/Abstraction/Composer/PackageDiscovery.php
@@ -2,12 +2,18 @@
 
 namespace Wordless\Contracts\Abstraction\Composer;
 
+use Composer\Package\CompletePackage;
 use Composer\Script\Event;
 
 trait PackageDiscovery
 {
     public static function discover(Event $event)
     {
-        dump($event->getComposer()->getRepositoryManager()->getLocalRepository()->getPackages());
+        /** @var CompletePackage[] $packagesList */
+        $packagesList = $event->getComposer()->getRepositoryManager()->getLocalRepository()->getPackages();
+
+        foreach ($packagesList as $package) {
+            dump($package->getExtra());
+        }
     }
 }
