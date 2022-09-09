da819ea75315a5a9bee416376a7398fd4d60c81a
Thales Matias <thales.matias@infobase.com.br>
2022-09-08 21:14:09 -0300

    Composer script to register version
    Registering version of project installed.

--- src/Abstractions/Composer.php
+++ src/Abstractions/Composer.php
@@ -9,7 +9,9 @@ use Composer\Installer\PackageEvent;
 use Composer\Package\CompletePackage;
 use Wordless\Contracts\Abstraction\Composer\ManagePlugin;
 use Wordless\Contracts\Abstraction\Composer\PackageDiscovery;
+use Wordless\Exceptions\PathNotFoundException;
 use Wordless\Helpers\ProjectPath;
+use Wordless\Helpers\Str;
 
 class Composer
 {
@@ -25,6 +27,29 @@ class Composer
     public static function isPackageInstalled(string $package_full_name): bool
     {
         return InstalledVersions::isInstalled($package_full_name);
+    }
+
+    /**
+     * @param PackageEvent $composerEvent
+     * @return void
+     * @throws PathNotFoundException
+     */
+    public static function saveInstalledVersion(PackageEvent $composerEvent)
+    {
+        $package = self::extractPackageFromEvent($composerEvent);
+        $style_css_path = ProjectPath::wpThemes('wordless/style.css');
+        $style_css_content = file_get_contents($style_css_path);
+
+        if (!Str::contains($style_css_content, 'Version:')) {
+            file_put_contents(
+                $style_css_path,
+                str_replace(
+                    '*/',
+                    PHP_EOL . "Version: {$package->getVersion()}" . PHP_EOL . '*/',
+                    $style_css_content
+                )
+            );
+        }
     }
 
     private static function extractPackageFromEvent(PackageEvent $composerEvent): ?CompletePackage
