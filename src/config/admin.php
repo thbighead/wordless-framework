cf3e5b23b2e86befa5f80d8e87a923e7f6bb98c1
Thales Matias <thales.matias@infobase.com.br>
2022-10-03 12:14:25 -0300

    Renaming class
    - Also configuring it to be called
    - Also calling missing WordlessVersionOnAdmin hooker class to be called

--- src/config/admin.php
+++ src/config/admin.php
@@ -5,13 +5,13 @@ use Wordless\Abstractions\WpSpeedUp;
 use Wordless\Helpers\Roles;
 use Wordless\Hookers\HideDiagnosticsFromUserRoles;
 use Wordless\Hookers\DoNotLoadWpAdminBarOutsidePanel;
-use Wordless\Hookers\SwitchImagickByGd;
+use Wordless\Hookers\ChooseImageEditor;
 
 return [
     WpSpeedUp::REMOVE_WP_EMOJIS_CONFIG_KEY => false,
     WpSpeedUp::SPEED_UP_WP_CONFIG_KEY => true,
     DoNotLoadWpAdminBarOutsidePanel::SHOW_WP_ADMIN_BAR_OUTSIDE_PANEL_CONFIG_KEY => true,
-    SwitchImagickByGd::IMAGE_LIBRARY_CONFIG_KEY => SwitchImagickByGd::IMAGE_LIBRARY_CONFIG_VALUE_IMAGICK,
+    ChooseImageEditor::IMAGE_LIBRARY_CONFIG_KEY => ChooseImageEditor::IMAGE_LIBRARY_CONFIG_VALUE_IMAGICK,
     HideDiagnosticsFromUserRoles::SHOW_DIAGNOSTICS_CONFIG_KEY => [
         Roles::ADMIN => true,
         Roles::AUTHOR => false,
