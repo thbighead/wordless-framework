4d949e3a78978b832c9daa66d26c41e948748cde
Thales Nathan <thales.matias@infobase.com.br>
2023-08-16 15:57:39 -0300

    Fixing logic and names
    I misunderstood those freaking names.

--- src/Abstractions/AjaxHooker.php
+++ src/Abstractions/AjaxHooker.php
@@ -7,11 +7,11 @@ abstract class AjaxHooker extends Hooker
     /**
      * Define wp_ajax_ prefixed hook to let AJAX be called from admin panel (only logged in)
      */
-    protected const AVAILABLE_TO_ADMIN_PANEL = false;
+    protected const AVAILABLE_AUTHENTICATED = false;
     /**
      * Define wp_ajax_nopriv_ prefixed hook to let AJAX be called from frontend application (log in not an obligation)
      */
-    protected const AVAILABLE_TO_FRONTEND = true;
+    protected const AVAILABLE_UNAUTHENTICATED = true;
     private const WP_AJAX_NOPRIV_PREFIX = 'wp_ajax_nopriv_';
     private const WP_AJAX_PREFIX = 'wp_ajax_';
 
@@ -19,18 +19,18 @@ abstract class AjaxHooker extends Hooker
     {
         $hook_addition_function = 'add_' . static::TYPE;
 
-        if (static::AVAILABLE_TO_FRONTEND) {
+        if (static::AVAILABLE_UNAUTHENTICATED) {
             $hook_addition_function(
-                self::WP_AJAX_PREFIX . static::HOOK,
+                self::WP_AJAX_NOPRIV_PREFIX . static::HOOK,
                 [static::class, static::FUNCTION],
                 static::HOOK_PRIORITY,
                 static::ACCEPTED_NUMBER_OF_ARGUMENTS
             );
         }
 
-        if (static::AVAILABLE_TO_ADMIN_PANEL) {
+        if (static::AVAILABLE_AUTHENTICATED) {
             $hook_addition_function(
-                self::WP_AJAX_NOPRIV_PREFIX . static::HOOK,
+                self::WP_AJAX_PREFIX . static::HOOK,
                 [static::class, static::FUNCTION],
                 static::HOOK_PRIORITY,
                 static::ACCEPTED_NUMBER_OF_ARGUMENTS
