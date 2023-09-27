a09e14deeaf82e4bfb192523f2ea43a28dbba22b
Thales Nathan <thales.matias@infobase.com.br>
2023-09-27 11:33:50 -0300

    WIP

--- src/Application/Helpers/Arr.php
+++ src/Application/Helpers/Arr.php
@@ -2,6 +2,8 @@
 
 namespace Wordless\Application\Helpers;
 
+use Wordless\Application\Helpers\Arr\Exceptions\FailedToFindArrayKey;
+
 class Arr
 {
     public static function except(array $array, array $except_keys): array
@@ -18,20 +20,42 @@ class Arr
         return array_diff_key($array, $except_array);
     }
 
+    public static function get(array $array, int|string $key, mixed $default = null): mixed
+    {
+        try {
+            return static::getOrFail($array, $key);
+        } catch (FailedToFindArrayKey) {
+            return $default;
+        }
+    }
+
+    /**
+     * @param array $array
+     * @param int|string $key
+     * @return mixed
+     * @throws FailedToFindArrayKey
+     */
+    public static function getOrFail(array $array, int|string $key): mixed
+    {
+        $key = (string)$key;
+        $key_pathing = explode('.', $key);
+        $first_key = array_shift($key_pathing);
+        $pointer = $array[$first_key] ?? throw new FailedToFindArrayKey($array, $key, $first_key);
+
+        foreach ($key_pathing as $key_path) {
+            if (!isset($pointer[$key_path])) {
+                throw new FailedToFindArrayKey($array, $key, $key_path);
+            }
+
+            $pointer = $pointer[$key_path];
+        }
+
+        return $pointer;
+    }
+
     public static function isAssociative(array $array): bool
     {
         return array_keys($array) !== range(0, count($array) - 1);
-    }
-
-    public static function searchValue(array $array, $value): int|string|null
-    {
-        foreach ($array as $key => $item) {
-            if ($item === $value) {
-                return $key;
-            }
-        }
-
-        return null;
     }
 
     public static function recursiveJoin(array ...$arrays): array
@@ -60,6 +84,21 @@ class Arr
         }
 
         return $joined_array;
+    }
+
+    public static function only(array $array, array $only_keys): array
+    {
+        $filtered_array = [];
+
+        foreach ($only_keys as $key_to_filter) {
+            try {
+                $filtered_array[$key_to_filter] = static::getOrFail($array, $key_to_filter);
+            } catch (FailedToFindArrayKey) {
+                continue;
+            }
+        }
+
+        return $filtered_array;
     }
 
     public static function wrap($something): array
