bee2a7eb87fd6549539982ea2b7d7c9f2c242ac5
Thales Nathan <thales.matias@infobase.com.br>
2024-01-16 14:51:18 -0300

    Fixing exception class

--- src/Application/Helpers/DirectoryFiles/Exceptions/FailedToCreateSymlink.php
+++ src/Application/Helpers/DirectoryFiles/Exceptions/FailedToCreateSymlink.php
@@ -4,43 +4,21 @@ namespace Wordless\Application\Helpers\DirectoryFiles\Exceptions;
 
 use ErrorException;
 use Throwable;
+use Wordless\Infrastructure\Enums\ExceptionCode;
 
 class FailedToCreateSymlink extends ErrorException
 {
-    private string $link_relative_path;
-    private string $target_relative_path;
-    private string $from_absolute_path;
-
     public function __construct(
-        string    $link_relative_path,
-        string    $target_relative_path,
-        string    $from_absolute_path,
-        Throwable $previous = null
+        public readonly string $link_relative_path,
+        public readonly string $target_relative_path,
+        public readonly string $from_absolute_path,
+        Throwable              $previous = null
     )
     {
-        $this->link_relative_path = $link_relative_path;
-        $this->target_relative_path = $target_relative_path;
-        $this->from_absolute_path = $from_absolute_path;
-
         parent::__construct(
             "Failed to create $this->link_relative_path symlink pointing to $this->target_relative_path from $this->from_absolute_path",
-            0,
-            $previous
+            ExceptionCode::intentional_interrupt->value,
+            previous: $previous
         );
-    }
-
-    public function getFromAbsolutePath(): string
-    {
-        return $this->from_absolute_path;
-    }
-
-    public function getLinkRelativePath(): string
-    {
-        return $this->link_relative_path;
-    }
-
-    public function getTargetRelativePath(): string
-    {
-        return $this->target_relative_path;
     }
 }
