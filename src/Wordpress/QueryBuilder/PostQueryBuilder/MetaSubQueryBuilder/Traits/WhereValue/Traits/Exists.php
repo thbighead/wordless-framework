b08ddd334f1c3d69002b7156e97e723eb8167968
Thales Nathan <thales.matias@infobase.com.br>
2024-01-22 09:45:54 -0300

    Finishing MetaSubQueryBuilder?

--- src/Wordpress/QueryBuilder/PostQueryBuilder/MetaSubQueryBuilder/Traits/WhereValue/Traits/Exists.php
+++ src/Wordpress/QueryBuilder/PostQueryBuilder/MetaSubQueryBuilder/Traits/WhereValue/Traits/Exists.php
@@ -2,14 +2,60 @@
 
 namespace Wordless\Wordpress\QueryBuilder\PostQueryBuilder\MetaSubQueryBuilder\Traits\WhereValue\Traits;
 
+use Carbon\Carbon;
 use Wordless\Wordpress\QueryBuilder\PostQueryBuilder\MetaSubQueryBuilder\Enums\Compare;
 use Wordless\Wordpress\QueryBuilder\PostQueryBuilder\MetaSubQueryBuilder\Enums\Type;
 
 trait Exists
 {
-    public function whereValueExists(string|int|float|bool|null $value = null): static
+    public function whereBinaryValueExists(bool $value): static
     {
-        $this->arguments[] = $this->mountArgument($value, Type::char, compare: Compare::exists);
+        return $this->whereValueExists($value, Type::binary);
+    }
+
+    public function whereCharValueExists(string $value): static
+    {
+        return $this->whereValueExists($value, Type::char);
+    }
+
+    public function whereDateValueExists(Carbon $value): static
+    {
+        return $this->whereValueExists($value->toDateString(), Type::date);
+    }
+
+    public function whereDateTimeValueExists(Carbon $value): static
+    {
+        return $this->whereValueExists($value->toDateTimeString(), Type::datetime);
+    }
+
+    public function whereDecimalValueExists(float $value): static
+    {
+        return $this->whereValueExists($value, Type::decimal);
+    }
+
+    public function whereNumericValueExists(int|float $value): static
+    {
+        return $this->whereValueExists($value, Type::numeric);
+    }
+
+    public function whereSignedValueExists(int $value): static
+    {
+        return $this->whereValueExists($value, Type::signed);
+    }
+
+    public function whereTimeValueExists(Carbon $value): static
+    {
+        return $this->whereValueExists($value->toTimeString(), Type::time);
+    }
+
+    public function whereUnsignedValueExists(int $value): static
+    {
+        return $this->whereValueExists($value, Type::unsigned);
+    }
+
+    private function whereValueExists(string|int|float|bool $value, Type $valueType): static
+    {
+        $this->arguments[] = $this->mountArgument($value, $valueType, compare: Compare::exists);
 
         return $this;
     }
