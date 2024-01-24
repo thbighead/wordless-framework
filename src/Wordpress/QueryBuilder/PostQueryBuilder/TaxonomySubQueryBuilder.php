c835afa93024a89b730185aa1fbac1348c0e8546
Thales Nathan <thales.matias@infobase.com.br>
2024-01-24 15:40:40 -0300

    Finished TaxonomySubQueryBuilder?

--- src/Wordpress/QueryBuilder/PostQueryBuilder/TaxonomySubQueryBuilder.php
+++ src/Wordpress/QueryBuilder/PostQueryBuilder/TaxonomySubQueryBuilder.php
@@ -6,13 +6,28 @@ use Wordless\Infrastructure\Wordpress\QueryBuilder\PostSubQueryBuilder\Recursive
 use Wordless\Wordpress\QueryBuilder\PostQueryBuilder\TaxonomySubQueryBuilder\Enums\Field;
 use Wordless\Wordpress\QueryBuilder\PostQueryBuilder\TaxonomySubQueryBuilder\Enums\Operator;
 use Wordless\Wordpress\QueryBuilder\PostQueryBuilder\TaxonomySubQueryBuilder\Traits\ArgumentMounter;
+use Wordless\Wordpress\QueryBuilder\PostQueryBuilder\TaxonomySubQueryBuilder\Traits\WhereTermId;
+use Wordless\Wordpress\QueryBuilder\PostQueryBuilder\TaxonomySubQueryBuilder\Traits\WhereTermName;
+use Wordless\Wordpress\QueryBuilder\PostQueryBuilder\TaxonomySubQueryBuilder\Traits\WhereTermSlug;
+use Wordless\Wordpress\QueryBuilder\PostQueryBuilder\TaxonomySubQueryBuilder\Traits\WhereTermTaxonomyId;
 
 class TaxonomySubQueryBuilder extends RecursiveSubQueryBuilder
 {
     use ArgumentMounter;
+    use WhereTermId;
+    use WhereTermName;
+    use WhereTermSlug;
+    use WhereTermTaxonomyId;
 
     final public const ARGUMENT_KEY = 'tax_query';
 
+    /**
+     * @param Field $termField
+     * @param string|int|string[]|int[] $term
+     * @param string|null $taxonomy
+     * @param Operator $operator
+     * @return $this
+     */
     private function where(
         Field            $termField,
         string|int|array $term,
@@ -25,6 +40,13 @@ class TaxonomySubQueryBuilder extends RecursiveSubQueryBuilder
         return $this;
     }
 
+    /**
+     * @param Field $termField
+     * @param string|int|string[]|int[] $term
+     * @param string|null $taxonomy
+     * @param Operator $operator
+     * @return $this
+     */
     private function whereIncludingChildren(
         Field            $termField,
         string|int|array $term,
--- src/Wordpress/QueryBuilder/PostQueryBuilder/TaxonomySubQueryBuilder/Traits/WhereTermTaxonomyId.php
+++ src/Wordpress/QueryBuilder/PostQueryBuilder/TaxonomySubQueryBuilder/Traits/WhereTermTaxonomyId.php
@@ -2,7 +2,109 @@
 
 namespace Wordless\Wordpress\QueryBuilder\PostQueryBuilder\TaxonomySubQueryBuilder\Traits;
 
+use Wordless\Application\Helpers\Arr;
+use Wordless\Wordpress\QueryBuilder\PostQueryBuilder\TaxonomySubQueryBuilder\Enums\Field;
+use Wordless\Wordpress\QueryBuilder\PostQueryBuilder\TaxonomySubQueryBuilder\Enums\Operator;
+
 trait WhereTermTaxonomyId
 {
+    public function whereTermTaxonomyIdAnd(int $term_taxonomy_id, int ...$term_taxonomy_ids): static
+    {
+        return $this->whereTermTaxonomyId(Arr::prepend($term_taxonomy_ids, $term_taxonomy_id), Operator::and);
+    }
 
+    public function whereTermTaxonomyIdAndIncludingChildren(int $term_taxonomy_id, int ...$term_taxonomy_ids): static
+    {
+        return $this->whereTermTaxonomyIdIncludingChildren(
+            Arr::prepend($term_taxonomy_ids, $term_taxonomy_id),
+            Operator::and
+        );
+    }
+
+    public function whereTermTaxonomyIdExists(int $term_taxonomy_id, int ...$term_taxonomy_ids): static
+    {
+        return $this->whereTermTaxonomyId(
+            Arr::prepend($term_taxonomy_ids, $term_taxonomy_id),
+            Operator::exists
+        );
+    }
+
+    public function whereTermTaxonomyIdExistsIncludingChildren(
+        int $term_taxonomy_id,
+        int ...$term_taxonomy_ids
+    ): static
+    {
+        return $this->whereTermTaxonomyIdIncludingChildren(
+            Arr::prepend($term_taxonomy_ids, $term_taxonomy_id),
+            Operator::exists
+        );
+    }
+
+    public function whereTermTaxonomyIdIn(int $term_taxonomy_id, int ...$term_taxonomy_ids): static
+    {
+        return $this->whereTermTaxonomyId(Arr::prepend($term_taxonomy_ids, $term_taxonomy_id));
+    }
+
+    public function whereTermTaxonomyIdInIncludingChildren(int $term_taxonomy_id, int ...$term_taxonomy_ids): static
+    {
+        return $this->whereTermTaxonomyIdIncludingChildren(Arr::prepend($term_taxonomy_ids, $term_taxonomy_id));
+    }
+
+    public function whereTermTaxonomyIdNotExists(int $term_taxonomy_id, int ...$term_taxonomy_ids): static
+    {
+        return $this->whereTermTaxonomyId(
+            Arr::prepend($term_taxonomy_ids, $term_taxonomy_id),
+            Operator::not_exists
+        );
+    }
+
+    public function whereTermTaxonomyIdNotExistsIncludingChildren(
+        int $term_taxonomy_id,
+        int ...$term_taxonomy_ids
+    ): static
+    {
+        return $this->whereTermTaxonomyIdIncludingChildren(
+            Arr::prepend($term_taxonomy_ids, $term_taxonomy_id),
+            Operator::not_exists
+        );
+    }
+
+    public function whereTermTaxonomyIdNotIn(int $term_taxonomy_id, int ...$term_taxonomy_ids): static
+    {
+        return $this->whereTermTaxonomyId(
+            Arr::prepend($term_taxonomy_ids, $term_taxonomy_id),
+            Operator::not_in
+        );
+    }
+
+    public function whereTermTaxonomyIdNotInIncludingChildren(int $term_taxonomy_id, int ...$term_taxonomy_ids): static
+    {
+        return $this->whereTermTaxonomyIdIncludingChildren(
+            Arr::prepend($term_taxonomy_ids, $term_taxonomy_id),
+            Operator::not_in
+        );
+    }
+
+    /**
+     * @param int|int[] $term_taxonomy_id
+     * @param Operator $operator
+     * @return $this
+     */
+    private function whereTermTaxonomyId(int|array $term_taxonomy_id, Operator $operator = Operator::in): static
+    {
+        return $this->where(Field::term_taxonomy_id, $term_taxonomy_id, operator: $operator);
+    }
+
+    /**
+     * @param int|int[] $term_taxonomy_id
+     * @param Operator $operator
+     * @return $this
+     */
+    private function whereTermTaxonomyIdIncludingChildren(
+        int|array $term_taxonomy_id,
+        Operator  $operator = Operator::in
+    ): static
+    {
+        return $this->whereIncludingChildren(Field::term_taxonomy_id, $term_taxonomy_id, operator: $operator);
+    }
 }
