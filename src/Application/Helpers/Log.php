6bfa07ac1e8c56bd05c00c315d570811f7561eeb
Fabio Baptista <fabio.ferreira@infobase.com.br>
2024-01-26 15:24:25 -0300

    wip log

--- src/Application/Helpers/Log.php
+++ src/Application/Helpers/Log.php
@@ -1,26 +1,90 @@
-<?php declare(strict_types=1);
+<?php
 
 namespace Wordless\Application\Helpers;
 
-use Wordless\Application\Helpers\Log\Enums\Type;
-use Wordless\Application\Helpers\Log\Traits\Internal;
+use Monolog\Logger;
+use Wordless\Application\Helpers\Log\Adapters\LogFormatter;
+use Wordless\Application\Helpers\ProjectPath\Exceptions\PathNotFoundException;
 
 class Log
 {
-    use Internal;
+    private Logger $logger;
+    private const LOG_PATH = 'test.log';
 
-    final public static function error(string $message): void
+    public function __construct()
     {
-        self::write($message, Type::ERROR);
+        $this->logger = new Logger(
+            Environment::get('APP_NAME', 'wordless')
+            . '.'
+            . Environment::get('APP_ENV')
+        );
+
+        try {
+            $path = ProjectPath::wpContent(self::LOG_PATH);
+        } catch (PathNotFoundException $exception) {
+            DirectoryFiles::createFileAt($path = $exception->getPath());
+        }
+
+        $this->logger->pushHandler(new LogFormatter($path));
+        $this->logger->pushHandler(new LogFormatter($path));
     }
 
-    final public static function info(string $message): void
+    public static function alert($message, array $context = [], bool $json_format = false): void
     {
-        self::write($message, Type::INFO);
+        (new self)->writeLog(__FUNCTION__, $message, $context, $json_format);
     }
 
-    final public static function warning(string $message): void
+    public static function critical($message, array $context = [], bool $json_format = false): void
     {
-        self::write($message, Type::WARNING);
+        (new self)->writeLog(__FUNCTION__, $message, $context, $json_format);
+    }
+
+    public static function debug($message, array $context = [], bool $json_format = false): void
+    {
+        (new self)->writeLog(__FUNCTION__, $message, $context, $json_format);
+    }
+
+    public static function emergency($message, array $context = [], bool $json_format = false): void
+    {
+        (new self)->writeLog(__FUNCTION__, $message, $context, $json_format);
+    }
+
+    public static function error($message, array $context = [], bool $json_format = false): void
+    {
+        (new self)->writeLog(__FUNCTION__, $message, $context, $json_format);
+    }
+
+    public static function info($message, array $context = [], bool $json_format = false): void
+    {
+        (new self)->writeLog(__FUNCTION__, $message, $context, $json_format);
+    }
+
+    public static function notice($message, array $context = [], bool $json_format = false): void
+    {
+        (new self)->writeLog(__FUNCTION__, $message, $context, $json_format);
+    }
+
+    public static function warning($message, array $context = [], bool $json_format = false): void
+    {
+        (new self)->writeLog(__FUNCTION__, $message, $context, $json_format);
+    }
+
+    protected function formatMessage($message, bool $json_format = false): false|string|null
+    {
+        if (is_array($message)) {
+            return var_export($message, true);
+        } elseif ($json_format === true || is_object($message)) {
+            return json_encode($message);
+        }
+
+        return (string)$message;
+    }
+
+    protected function writeLog($level, $message, $context, bool $json_format): void
+    {
+        $this->logger->{$level}(
+            $this->formatMessage($message, $json_format),
+            $context
+        );
     }
 }
---
+++ src/Application/Helpers/Log/Adapters/LogFormatter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Wordless\Application\Helpers\Log\Adapters;
+
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\StreamHandler;
+use Monolog\Level;
+use Monolog\Logger;
+
+class LogFormatter extends StreamHandler
+{
+
+    public const SIMPLE_FORMAT = "[%datetime%] %channel%.%level_name%: %message% %context% %extra%\n";
+
+    public function __construct($stream, int|string|Level $level = Level::Debug, bool $bubble = true, ?int $filePermission = null, bool $useLocking = false)
+    {
+        parent::__construct($stream, $level, $bubble, $filePermission, $useLocking);
+        $this->setFormatter($this->mountOutputFormatter());
+    }
+
+    private function mountOutputFormatter(): LineFormatter
+    {
+        return new LineFormatter(self::SIMPLE_FORMAT, 'd-M-Y H:m:s', false, true);
+    }
+}
