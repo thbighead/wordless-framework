3e085781e2b1d32e466262c8d9e988da008dec9f
Thales Nathan <thales.matias@infobase.com.br>
2024-02-07 13:34:50 -0300

    Refactoring Config Helper usage
    Finished

--- src/Application/Helpers/Timezone.php
+++ src/Application/Helpers/Timezone.php
@@ -2,6 +2,7 @@
 
 namespace Wordless\Application\Helpers;
 
+use Wordless\Application\Commands\ConfigureDateOptions;
 use Wordless\Application\Helpers\Config\Contracts\Subjectable\DTO\ConfigSubjectDTO\Exceptions\EmptyConfigKey;
 use Wordless\Application\Helpers\Config\Exceptions\InvalidConfigKey;
 use Wordless\Application\Helpers\ProjectPath\Exceptions\PathNotFoundException;
@@ -18,7 +19,7 @@ class Timezone
      */
     public static function raw(): string
     {
-        return Config::wordpressAdmin()->ofKey('datetime')
+        return Config::wordpressAdmin()->ofKey(ConfigureDateOptions::CONFIG_KEY_ADMIN_DATETIME)
             ->getOrFail(self::CONFIG_KEY);
     }
 
