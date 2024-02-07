5106303a356f12d5f0db0b18f672df4c62a686e5
Thales Nathan <thales.matias@infobase.com.br>
2024-02-07 15:16:20 -0300

    Refactoring not finished tests
    They should be finished now.

--- tests/Unit/TaxonomyQueryBuilderTest/WhereManagePermissionTest.php
+++ tests/Unit/TaxonomyQueryBuilderTest/WhereManagePermissionTest.php
@@ -2,13 +2,9 @@
 
 namespace Wordless\Tests\Unit\TaxonomyQueryBuilderTest;
 
-use ReflectionException;
 use Wordless\Tests\Unit\TaxonomyQueryBuilderTest\WhereManagePermissionTest\Traits\AndTests;
 use Wordless\Tests\Unit\TaxonomyQueryBuilderTest\WhereManagePermissionTest\Traits\NotTests;
 use Wordless\Tests\Unit\TaxonomyQueryBuilderTest\WhereManagePermissionTest\Traits\OrTests;
-use Wordless\Tests\WordlessTestCase\TaxonomyBuilderTestCase;
-use Wordless\Wordpress\QueryBuilder\TaxonomyQueryBuilder;
-use Wordless\Wordpress\QueryBuilder\TaxonomyQueryBuilder\AndComparison;
 
 trait WhereManagePermissionTest
 {
