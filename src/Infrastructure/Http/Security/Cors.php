2d61b199bae435f2b700b28ceedd615f66f25c74
Thales Nathan <thales.matias@infobase.com.br>
2024-07-04 11:50:17 -0300

    First CORS implementation
    PENDING TEST

--- src/Infrastructure/Http/Security/Cors.php
+++ src/Infrastructure/Http/Security/Cors.php
@@ -2,19 +2,59 @@
 
 namespace Wordless\Infrastructure\Http\Security;
 
+use Fruitcake\Cors\CorsService;
+use Symfony\Component\HttpFoundation\Response;
+use Wordless\Application\Helpers\Config;
+use Wordless\Application\Helpers\Config\Contracts\Subjectable\DTO\ConfigSubjectDTO\Exceptions\EmptyConfigKey;
+use Wordless\Application\Helpers\ProjectPath\Exceptions\PathNotFoundException;
 use Wordless\Application\Libraries\DesignPattern\Singleton;
+use Wordless\Infrastructure\Http\Request;
 
 final class Cors extends Singleton
 {
     public const CONFIG_KEY = 'cors';
 
+    private Request $request;
+    private CorsService $service;
+
     public static function enable(): void
     {
-        self::getInstance();
+        $singleton = self::getInstance();
+
+        if ($singleton->isCorsRequest()) {
+            $singleton->handleCorsRequest();
+        }
     }
 
+    /**
+     * @throws EmptyConfigKey
+     * @throws PathNotFoundException
+     */
     private function __construct()
     {
+        $this->service = new CorsService(Config::wordlessCors()->get());
+        $this->request = Request::getInstance();
+
         parent::__construct();
+    }
+
+    private function handleCorsRequest(): void
+    {
+        if ($this->service->isPreflightRequest($this->request)) {
+            $this->handlePreflightRequest();
+        }
+
+        $this->service->addActualRequestHeaders(new Response, $this->request)->sendHeaders();
+    }
+
+    private function handlePreflightRequest(): void
+    {
+        $this->service->handlePreflightRequest($this->request)->send();
+        die;
+    }
+
+    private function isCorsRequest(): bool
+    {
+        return $this->service->isCorsRequest($this->request);
     }
 }
