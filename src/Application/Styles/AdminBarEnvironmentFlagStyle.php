66c6e3590e2e02c14d7cd2bbe6769ea3d34be381
Thales Nathan <thales.matias@infobase.com.br>
2024-08-09 13:08:30 -0300

    Renaming listener and introducing enqueueable style

---
+++ src/Application/Styles/AdminBarEnvironmentFlagStyle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Wordless\Application\Styles;
+
+use Wordless\Infrastructure\Wordpress\EnqueueableAsset\EnqueueableStyle;
+
+class AdminBarEnvironmentFlagStyle extends EnqueueableStyle
+{
+    protected static function relativeFilepath(): string
+    {
+        // TODO: Implement relativeFilepath() method.
+    }
+}
