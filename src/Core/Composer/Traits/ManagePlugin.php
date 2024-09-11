f6d41b65cea11bf3e3fa6c8bd6bee73f8a5e6319
Thales Nathan <thales.matias@infobase.com.br>
2024-09-11 17:45:27 -0300

    Fixing tty
    Fixing the following error during composer scripts:  TTY mode requires
    /dev/tty to be read/writable.

--- src/Core/Composer/Traits/ManagePlugin.php
+++ src/Core/Composer/Traits/ManagePlugin.php
@@ -42,7 +42,7 @@ trait ManagePlugin
         if (is_file($autoload_path)) {
             require_once $autoload_path;
 
-            exec('php console wp:run "db check"', result_code: $db_checking);
+            exec('php console wp:run "db check" --no-tty', result_code: $db_checking);
 
             if ($db_checking) {
                 static::getIo()->write('Database not ok. Skipping.');
