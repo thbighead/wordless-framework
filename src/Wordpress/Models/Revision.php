bbcc45f601eb92416a443beb5d9544a7f09532d2
Thales Nathan <thales.matias@infobase.com.br>
2024-09-25 17:27:27 -0300

    PostQueryBuilder was not working
    - Fixing InitializingModelWithWrongPostType error on mounting message.
    The following error should not appear anymore: PHP Parse error:  syntax
    error, unexpected token "}", expecting "->" or "?->" or "{" or "[" in
    /var/www/vendor/infobaseit/wordless-framework/src/Wordpress/Models/Post/Exceptions/InitializingModelWithWrongPostType.php
    on line 22
    - Attachment and Revision models should extend from Post not CustomPost
    - Post model should behave only for posts of type posts (repository was
    trying to get any type)
    - Page, Posts, PaginationArgumentsBuilder and PostQueryBuilder must use
    WP_Post instead of Post. If not, we can't use them for any type of WP
    post

--- src/Wordpress/Models/Revision.php
+++ src/Wordpress/Models/Revision.php
@@ -2,10 +2,9 @@
 
 namespace Wordless\Wordpress\Models;
 
-use Wordless\Infrastructure\Wordpress\CustomPost;
 use Wordless\Wordpress\Models\PostType\Enums\StandardType;
 
-class Revision extends CustomPost
+class Revision extends Post
 {
     final protected const TYPE_KEY = StandardType::revision->name;
 }
