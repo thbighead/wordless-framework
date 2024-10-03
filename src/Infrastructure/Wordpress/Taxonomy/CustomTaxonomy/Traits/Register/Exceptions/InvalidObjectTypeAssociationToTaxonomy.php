e74c4a130a25292902f2623831614c9909114964
Thales Nathan <thales.matias@infobase.com.br>
2024-10-03 15:30:41 -0300

    Fixing custom taxonomy registration
    - updating availableTo CustomTaxonomy abstract class' Register trait
    method docblock to include CustomPost and StandardType
    - new InvalidObjectTypeAssociationToTaxonomy exception introduced to be
    thrown when none of the types listed in availableTo is passed as array
    item
    - updating docblocks with new thrown exception possibility

---
+++ src/Infrastructure/Wordpress/Taxonomy/CustomTaxonomy/Traits/Register/Exceptions/InvalidObjectTypeAssociationToTaxonomy.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Wordless\Infrastructure\Wordpress\Taxonomy\CustomTaxonomy\Traits\Register\Exceptions;
+
+use InvalidArgumentException;
+use Throwable;
+use Wordless\Application\Helpers\GetType;
+use Wordless\Infrastructure\Enums\ExceptionCode;
+
+class InvalidObjectTypeAssociationToTaxonomy extends InvalidArgumentException
+{
+    public function __construct(
+        public readonly mixed $objectType,
+        public readonly string $taxonomy_name_key,
+        ?Throwable $previous = null
+    )
+    {
+        parent::__construct($this->mountMessage(), ExceptionCode::development_error->value, $previous);
+    }
+
+    private function isObjectTypeStringCastable(): bool
+    {
+        return is_string($this->objectType) || GetType::isStringable($this->objectType);
+    }
+
+    private function mountMessage(): string
+    {
+        if ($this->isObjectTypeStringCastable()) {
+            return "$this->objectType cannot be associated to taxonomies (tried with $this->taxonomy_name_key).";
+        }
+
+        return "Invalid object type trying to be associated with taxonomy $this->taxonomy_name_key (it's not even stringable).";
+    }
+}
