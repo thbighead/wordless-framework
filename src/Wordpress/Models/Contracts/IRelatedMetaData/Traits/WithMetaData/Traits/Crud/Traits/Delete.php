ba759060d0dc3b49471cc35ba921f0ef53093ce3
Thales Nathan <thales.matias@infobase.com.br>
2024-10-21 13:23:21 -0300

    Implementing meta data deletion

--- src/Wordpress/Models/Contracts/IRelatedMetaData/Traits/WithMetaData/Traits/Crud/Traits/Delete.php
+++ src/Wordpress/Models/Contracts/IRelatedMetaData/Traits/WithMetaData/Traits/Crud/Traits/Delete.php
@@ -2,7 +2,25 @@
 
 namespace Wordless\Wordpress\Models\Contracts\IRelatedMetaData\Traits\WithMetaData\Traits\Crud\Traits;
 
+use Wordless\Wordpress\Models\Contracts\IRelatedMetaData\Traits\WithMetaData\Traits\Crud\Traits\Delete\Exceptions\FailedToDeleteMetaData;
+
 trait Delete
 {
-
+    /**
+     * @param string $meta_key
+     * @param string|null $meta_value
+     * @return void
+     * @throws FailedToDeleteMetaData
+     */
+    public function deleteMetaField(string $meta_key, ?string $meta_value = null): void
+    {
+        if (delete_metadata(
+                static::objectType()->name,
+                $this->ID,
+                $meta_key,
+                empty($meta_value) ? '' : $meta_value
+            ) === false) {
+            throw new FailedToDeleteMetaData($this, $meta_key, $meta_value);
+        }
+    }
 }
