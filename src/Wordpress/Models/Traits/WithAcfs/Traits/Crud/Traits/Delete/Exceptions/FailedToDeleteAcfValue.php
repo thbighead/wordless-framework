f4056614a43b3ff2158487dacbbc595c1527daff
Thales Nathan <thales.matias@infobase.com.br>
2024-10-28 13:36:23 -0300

    Fixing CreateOrUpdate methods
    Also reindenting FailedToDeleteAcfValue code (PHPSTorm ctrl+alt+L)

--- src/Wordpress/Models/Traits/WithAcfs/Traits/Crud/Traits/Delete/Exceptions/FailedToDeleteAcfValue.php
+++ src/Wordpress/Models/Traits/WithAcfs/Traits/Crud/Traits/Delete/Exceptions/FailedToDeleteAcfValue.php
@@ -7,15 +7,15 @@ use Throwable;
 
 class FailedToDeleteAcfValue extends ErrorException
 {
- public function __construct(
-     public readonly string $acf_reference,
-     public readonly int|string $acf_from_id,
-     ?Throwable $previous = null
- )
- {
-     parent::__construct(
-         "Failed to delete ACF referenced by '$this->acf_reference' for id $this->acf_from_id",
-         previous: $previous
-     );
- }
+    public function __construct(
+        public readonly string     $acf_reference,
+        public readonly int|string $acf_from_id,
+        ?Throwable                 $previous = null
+    )
+    {
+        parent::__construct(
+            "Failed to delete ACF referenced by '$this->acf_reference' for id $this->acf_from_id",
+            previous: $previous
+        );
+    }
 }
