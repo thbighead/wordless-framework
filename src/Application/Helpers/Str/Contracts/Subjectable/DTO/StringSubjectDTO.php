212b394d87a6ead61c6594ac9fc5a893a36352cc
Thales Matias <thales.matias@infobase.com.br>
2025-10-31 16:03:59 -0300

    Helpers improvements

--- src/Application/Helpers/Str/Contracts/Subjectable/DTO/StringSubjectDTO.php
+++ src/Application/Helpers/Str/Contracts/Subjectable/DTO/StringSubjectDTO.php
@@ -2,6 +2,9 @@
 
 namespace Wordless\Application\Helpers\Str\Contracts\Subjectable\DTO;
 
+use Wordless\Application\Helpers\Crypto;
+use Wordless\Application\Helpers\Crypto\Exceptions\DecryptionFailed;
+use Wordless\Application\Helpers\Environment\Exceptions\CannotResolveEnvironmentGet;
 use Wordless\Application\Helpers\Str\Contracts\Subjectable\DTO\StringSubjectDTO\Traits\HelperMethods;
 use Wordless\Application\Helpers\Str\Contracts\Subjectable\DTO\StringSubjectDTO\Traits\Internal;
 use Wordless\Infrastructure\Helper\Contracts\Subjectable\DTO\SubjectDTO;
@@ -10,4 +13,26 @@ final class StringSubjectDTO extends SubjectDTO
 {
     use HelperMethods;
     use Internal;
+
+    /**
+     * @return $this
+     * @throws DecryptionFailed
+     */
+    public function decrypt(): self
+    {
+        $this->subject = Crypto::decrypt($this->subject);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     * @throws CannotResolveEnvironmentGet
+     */
+    public function encrypt(): self
+    {
+        $this->subject = Crypto::encrypt($this->subject);
+
+        return $this;
+    }
 }
--- src/Application/Helpers/Str/Contracts/Subjectable/DTO/StringSubjectDTO/Traits/HelperMethods.php
+++ src/Application/Helpers/Str/Contracts/Subjectable/DTO/StringSubjectDTO/Traits/HelperMethods.php
@@ -2,21 +2,37 @@
 
 namespace Wordless\Application\Helpers\Str\Contracts\Subjectable\DTO\StringSubjectDTO\Traits;
 
+use Wordless\Application\Helpers\Arr;
+use Wordless\Application\Helpers\Arr\Contracts\Subjectable\DTO\ArraySubjectDTO;
 use Wordless\Application\Helpers\Str;
+use Wordless\Application\Helpers\Str\Contracts\Subjectable\DTO\StringSubjectDTO;
 use Wordless\Application\Helpers\Str\Contracts\Subjectable\DTO\StringSubjectDTO\Traits\HelperMethods\Traits\Boolean;
 use Wordless\Application\Helpers\Str\Contracts\Subjectable\DTO\StringSubjectDTO\Traits\HelperMethods\Traits\Mutators;
 use Wordless\Application\Helpers\Str\Contracts\Subjectable\DTO\StringSubjectDTO\Traits\HelperMethods\Traits\Substring;
 use Wordless\Application\Helpers\Str\Contracts\Subjectable\DTO\StringSubjectDTO\Traits\HelperMethods\Traits\WordCase;
 use Wordless\Application\Helpers\Str\Enums\Encoding;
 use Wordless\Application\Helpers\Str\Enums\Language;
+use Wordless\Application\Helpers\Str\Exceptions\JsonDecodeError;
 use Wordless\Application\Helpers\Str\Traits\Internal\Exceptions\FailedToCreateInflector;
 
+/**
+ * @mixin StringSubjectDTO
+ */
 trait HelperMethods
 {
     use Boolean;
     use Mutators;
     use Substring;
     use WordCase;
+
+    /**
+     * @return ArraySubjectDTO
+     * @throws JsonDecodeError
+     */
+    public function jsonDecode(): ArraySubjectDTO
+    {
+        return Arr::of(Str::jsonDecode($this->subject));
+    }
 
     public function length(?Encoding $encoding = null): int
     {
