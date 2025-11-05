e69ad937ad51e5f9c783c01f53d4c5ea7ad2d8dd
Thales Matias <thales.matias@infobase.com.br>
2025-11-05 09:06:29 -0300

    Fixing JwtTest problem

--- tests/WordlessTestCase.php
+++ tests/WordlessTestCase.php
@@ -3,11 +3,15 @@
 namespace Wordless\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Wordless\Application\Libraries\JWT\Token;
+use Wordless\Tests\Unit\JwtTest;
 
 abstract class WordlessTestCase extends TestCase
 {
     public static function setUpBeforeClass(): void
     {
+        $_ENV[Token::ENVIRONMENT_SIGN_VARIABLE] ??= base64_encode(JwtTest::JWT_4096_KEY);
+
         require_once __DIR__ . '/../wp/wp-core/wp-config.php';
 
         parent::setUpBeforeClass();
